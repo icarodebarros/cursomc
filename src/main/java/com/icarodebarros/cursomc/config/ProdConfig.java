5c3f0b942f0d0cfd35549c48b06b8ea2909d0cbf
Icaro de Barros <icarodebarros@gmail.com>
2020-07-10 15:01:03 -0300

    Criação arquivo config para produção

---
+++ src/main/java/com/icarodebarros/cursomc/config/ProdConfig.java
@@ -0,0 +1,39 @@
+package com.icarodebarros.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.icarodebarros.cursomc.services.DBService;
+import com.icarodebarros.cursomc.services.EmailService;
+import com.icarodebarros.cursomc.services.SmtpEmailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if (strategy == null || !strategy.equals("create")) {
+			return false;
+		}
+		dbService.instantiateTestDatabase();
+		
+		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+}
